91615471b5dc248e0088f59c339edbecf57cdc93
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2023-03-06 15:50:22 -0500

    Use state has been replaced with a reducer, added several cases to manage state.

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,13 +1,39 @@
-import { useState } from 'react';
+import {useReducer } from 'react';
+
+const initialState = {
+    count: 0,
+    numberToChangeBy: 1
+};
+
+const reducer = (state, action) =>  {
+    switch(action.type) {
+        case 'count':
+            return {...state, count: state.count};
+        case 'numberToChangeBy':
+            return {...state, numberToChangeBy: state.numberToChangeBy}
+        case 'incrementCount': {
+            return {...state, count: parseInt(state.count,10)+parseInt(state.numberToChangeBy,10)}
+        }
+        case 'decrementCount': {
+            return {...state, count: parseInt(state.count,10)-parseInt(state.numberToChangeBy,10)}
+        }
+        case 'changeNumber': {
+            return {...state, numberToChangeBy: action.value}
+        }
+        default:
+            throw new Error();
+    }
+}
+
 const Counter = () => {
-    const [count, setCount] = useState(0);
-    const [numberToChangeBy, setNumberToChangeBy] = useState(1);
+    const [state, dispatch] = useReducer(reducer, initialState);
+
     return (<div className="App">
-    <pre className="rainbow box text-center">Value {count}</pre>
+    <pre className="rainbow box text-center"> {state.count} </pre>
     <div className="flex gap center">
-        <button className="button-box" onClick={() => setCount(parseInt(count,10)+parseInt(numberToChangeBy,10))}><span className="huge">+</span>Increment by {numberToChangeBy}</button>
-        <button className="button-box" onClick={() => setCount(parseInt(count,10)-parseInt(numberToChangeBy,10))}><span className="huge">-</span>Decrement by {numberToChangeBy}</button></div>
-        <p className="flex gap center"><label className="button-box" htmlFor="number">Number to Increment/Decrement by </label><input className="input-box"  onChange={(e) => setNumberToChangeBy(e.target.value)} type="number" value={numberToChangeBy} name="number" id="number" /></p>
+        <button className="button-box" onClick={() => dispatch({type: 'incrementCount'})}><span className="huge">+</span>Increment by {state.numberToChangeBy}</button>
+        <button className="button-box" onClick={() => dispatch({type: 'decrementCount'})}><span className="huge">-</span>Decrement by {state.numberToChangeBy}</button></div>
+        <p className="flex gap center"><label className="button-box" htmlFor="number">Number to Increment/Decrement by </label><input className="input-box"  onChange={(e) => dispatch({type: 'changeNumber', value: e.target.value})} type="number" value={state.numberToChangeBy} name="number" id="number" /></p>
   </div>);
 }
 
